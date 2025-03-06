32b9953a236c95ddb1a57ba96f079b7c5f039b20
yannickn2622 <airsoftlife26@gmail.com>
2025-03-06 00:00:02 -0500

    Home Navigation Bar Established

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,51 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import {Tabs, Redirect} from 'expo-router'
+
+import {icons} from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return(
+    <View className="items-center justify-center">
+      <Image
+      source={icon} 
+      resizeMode="contain"
+      tintColor={color}
+      className="w-6 h-6" />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xxs`}>
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false}}
+      >
+
+        <Tabs.Screen 
+        name="home"
+        options={{ 
+          title: 'Home', 
+          headerShown: false, 
+          tabBarIcon: ({color, focused}) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused= {focused}
+                />
+          )
+         }} 
+        />
+      </Tabs>
+
+    </>
+  )
+}
+
+export default TabsLayout
--- app/index.tsx
+++ app/index.tsx
@@ -6,9 +6,9 @@ import "global.css";
 export default function App(){
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl"> Aora!</Text>
+            <Text className="text-5xl font-pblack"> AoraYao!</Text>
             <StatusBar style="auto" />
-            <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
+            <Link href="/home" style={{ color: 'blue'}}>Go to Home</Link>
         </View>
     );
 }
