672c26532c72a69f9bed11cf604ba85f12c2242d
yannickn2622 <airsoftlife26@gmail.com>
2025-03-05 19:26:33 -0500

    NativeWind and File Navigation Established - 30 Minutes Into CrashCourse

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
