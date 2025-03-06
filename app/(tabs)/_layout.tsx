b18a537c308e56ecb2a43dd49f7a79d31be863d6
yannickn2622 <airsoftlife26@gmail.com>
2025-03-06 00:45:28 -0500

    Navigation Bar Colors

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,7 +12,7 @@ const TabIcon = ({ icon, color, name, focused }) => {
       resizeMode="contain"
       tintColor={color}
       className="w-6 h-6" />
-      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xxs`}>
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xxs`} style={{color: color}}>
         {name}
       </Text>
     </View>
@@ -24,7 +24,16 @@ const TabsLayout = () => {
     <>
       <Tabs
         screenOptions={{
-          tabBarShowLabel: false}}
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarStyle:{
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height: 84, 
+          }
+        }}
       >
 
         <Tabs.Screen 
