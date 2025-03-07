818298080c82bce1e01cabbfe43483f5125ea806
yannickn2622 <airsoftlife26@gmail.com>
2025-03-07 00:00:13 -0500

    Started working on Auth Screens

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useState } from 'react'
+
+import {images} from '../../constants';
+import FormField from '../../components/FormField';
+
 
 const SignIn = () => {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+
+          <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
+
+          <Text className="text-white text-semibold mt-4 font-psemibold text-medium"> Log into Aora
+
+          </Text>
+
+          
+
+          <FormField 
+            title="Email" 
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            />
+
+          <FormField 
+            title="Password" 
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+            />
+        </View>
+
+      </ScrollView>
+
+    </SafeAreaView>
   )
 }
 
